051572836ffee543f1021f6bf863313af890c647
Valentin Churavy <v.churavy@gmail.com>
2021-06-09 15:18:41 +0200

    rename Session to LMP

--- src/LAMMPS.jl
+++ src/LAMMPS.jl
@@ -2,10 +2,10 @@ module LAMMPS
 
 include("api.jl")
 
-mutable struct Session
+mutable struct LMP
     handle::Ptr{Cvoid}
 
-    function Session()
+    function LMP()
         handle = API.lammps_open_no_mpi(0, C_NULL, C_NULL)
 
         this = new(handle)
@@ -15,9 +15,9 @@ mutable struct Session
         return this
     end
 end
-Base.unsafe_convert(::Type{Ptr{Cvoid}}, lmp::Session) = lmp.handle
+Base.unsafe_convert(::Type{Ptr{Cvoid}}, lmp::LMP) = lmp.handle
 
-function version(lmp::Session)
+function version(lmp::LMP)
     API.lammps_version(lmp)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test
 using LAMMPS
 
-let lmp = LAMMPS.Session()
+let lmp = LAMMPS.LMP()
     @test LAMMPS.API.lammps_version(lmp) >= 0
 end
