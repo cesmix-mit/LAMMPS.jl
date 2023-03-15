69d9ac6d1a28fa2687fa2350db3a00a7c7ba6094
Valentin Churavy <v.churavy@gmail.com>
2023-03-15 16:08:41 -0400

    finish example

--- src/external.jl
+++ src/external.jl
@@ -25,9 +25,14 @@ FixExternal(callback, lmp::LMP, name::String) = FixExternal(lmp::LMP, name::Stri
 
 function fix_external_callback(ctx::Ptr{Cvoid}, timestep::Int64, nlocal::Cint, ids::Ptr{Cint}, x::Ptr{Ptr{Float64}}, fexternal::Ptr{Ptr{Float64}})
     fix = Base.unsafe_pointer_to_objref(ctx)::FixExternal
+    nlocal = Int(nlocal)
 
-    @debug "Calling fix_external_callback on" fix timestep ids x fexternal
-    # TODO wrap as arrays
+    @debug "Calling fix_external_callback on" fix timestep nlocal
+    shape = (nlocal, 3)
+    x = unsafe_wrap(x, shape)
+    fexternal = unsafe_wrap(fexternal, shape)
+    ids = unsafe_wrap(ids, (nlocal,))
+
     fix.callback(fix, timestep, nlocal, ids, x, fexternal)
     return nothing
 end
@@ -36,7 +41,57 @@ function energy_global!(fix::FixExternal, energy)
     API.lammps_fix_external_set_energy_global(fix.lmp, fix.name, energy)
 end
 
+function neighbor_list(fix::FixExternal, request)
+    fix_neighbor_list(fix.lmp, fix.name, request)
+end
+
 # TODO
 # virial_global!
 # function virial_global!(fix::FixExternal, )
 
+function PairExternal(lmp, name, neigh_name, compute_force, compute_energy, cut_global)
+    cutsq = cut_global^2
+    FixExternal(lmp, name) do fix, timestep, nlocal, ids, x, fexternal
+        # Full neighbor list
+        idx = pair_neighbor_list(fix.lmp, neigh_name, 1, 0, 0)
+        nelements = API.lammps_neighlist_num_elements(fix.lmp, idx)
+
+        type = LAMMPS.extract_atom(lmp, "type")
+
+        # zero-out fexternal (noticed some undef memory)
+        fexternal .= 0
+
+        for ii in 1:nelements
+            iatom, neigh = LAMMPS.neighbors(lmp, idx, ii)
+            iatom += 1 # 1-based indexing
+            xtmp, ytmp, ztmp = view(x, :, iatom) # TODO SArray?
+            itype = type[iatom]
+            for jj in 1:length(neigh)
+                jatom = neigh[jj] + 1
+                delx = xtmp - x[1, jatom]
+                dely = ytmp - x[2, jatom]
+                delz = ztmp = x[3, jatom]
+                jtype = type[jatom]
+
+                rsq = delx*delx + dely*dely + delz*delz;
+
+                if rsq < cutsq
+                    fpair = compute_force(rsq, itype, jtype)
+
+                    fexternal[1, iatom] += delx*fpair
+                    fexternal[2, iatom] += dely*fpair
+                    fexternal[3, iatom] += delz*fpair
+                    if jatom <= nlocal # newton_pair
+                        fexternal[1, jatom] -= delx*fpair
+                        fexternal[2, jatom] -= dely*fpair
+                        fexternal[3, jatom] -= delz*fpair
+                    end
+
+                    # todo call compute_energy
+                    # TODO eflag 
+                    # TODO evflag
+                end
+            end
+        end
+    end
+end
