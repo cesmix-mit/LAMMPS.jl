5099cdf71d572b29d3c02d84f75ea5748b0c17e8
Valentin Churavy <v.churavy@gmail.com>
2023-03-15 17:05:57 -0400

    support newton_pair

--- src/LAMMPS.jl
+++ src/LAMMPS.jl
@@ -157,6 +157,7 @@ end
 function extract_global(lmp::LMP, name, dtype=nothing)
     if dtype === nothing
         dtype = API.lammps_extract_global_datatype(lmp, name)
+        dtype == -1 && error("Could not find dataype for global $name")
     end
     dtype = API._LMP_DATATYPE_CONST(dtype)
     type = dtype2type(dtype)
@@ -362,6 +363,12 @@ function extract_variable(lmp::LMP, name::String, group=nothing)
     else
         error("Unkown variable style $var")
     end
+end
+
+function extract_setting(lmp, name)
+    val = API.lammps_extract_setting(lmp, name)
+    val == -1 && error("Could not find setting $name")
+    return val
 end
 
 function gather_atoms(lmp::LMP, name, T, count)
