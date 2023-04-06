47c06a5015113ee36b5275b75cd138063ec626f9
Valentin Churavy <v.churavy@gmail.com>
2023-04-06 00:55:11 -0400

    Add extract_variable

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,30 @@ LMP(["-screen", "none"]) do lmp
     @test_throws ErrorException command(lmp, "nonsense")
 end
 
+@testset "Variables" begin
+    LMP(["-screen", "none"]) do lmp
+        command(lmp, "box tilt large")
+        command(lmp, "region cell block 0 1.0 0 1.0 0 1.0 units box")
+        command(lmp, "create_box 1 cell")
+        command(lmp, "create_atoms 1 random 10 1 NULL")
+
+        command(lmp, "variable var1 equal 1.0")
+        command(lmp, "variable var2 string \"hello\"")
+        command(lmp, "variable var3 atom x")
+        # TODO: x is 3d, how do we access more than the first dims
+        # command(lmp, "variable var4 vector sum(x)")
+
+        @test LAMMPS.extract_variable(lmp, "var1") == 1.0
+        @test LAMMPS.extract_variable(lmp, "var2") == "hello"
+        x = LAMMPS.extract_atom(lmp, "x")
+        x_var = LAMMPS.extract_variable(lmp, "var3")
+        @test length(x_var) == 10
+        @test x_var == x[1, :]
+        # x_sum = LAMMPS.extract_variable(lmp, "var4")
+        # @test x_sum == sum(x, dims=1)
+    end
+end
+
 MPI.mpiexec() do mpiexec
     @test success(pipeline(`$mpiexec -n 2 $(Base.julia_cmd()) mpitest.jl`, stderr=stderr, stdout=stdout))
 end
