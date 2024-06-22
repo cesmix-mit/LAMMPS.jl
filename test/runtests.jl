300347af794b4bab6feb27042fc81824b40e6a1c
Joroks <johannes@roksvaag.com>
2024-06-22 21:35:52 +0200

    add tests gather/scatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,4 +41,64 @@ end
     end
 end
 
+@testset "gather/scatter" begin
+    LMP(["-screen", "none"]) do lmp
+        # setting up example data
+        command(lmp, "atom_modify map yes")
+        command(lmp, "region cell block 0 3 0 3 0 3")
+        command(lmp, "create_box 1 cell")
+        command(lmp, "lattice sc 1")
+        command(lmp, "create_atoms 1 region cell")
+        command(lmp, "mass 1 1")
+
+        command(lmp, "compute pos all property/atom x y z")
+        command(lmp, "fix pos all ave/atom 10 1 10 c_pos[1] c_pos[2] c_pos[3]")
+
+        command(lmp, "run 10")
+        data = zeros(Float64, 3, 27)
+        subset = Int32.([2,5,10, 5])
+        data_subset = ones(Float64, 3, 4)
+
+        subset_bad1 = Int32.([28])
+        subset_bad2 = Int32.([0])
+        subset_bad_data = ones(Float64, 3,1)
+
+        @test_throws AssertionError gather(lmp, "x", Int32)
+        @test_throws AssertionError gather(lmp, "id", Float64)
+
+        @test_throws ErrorException gather(lmp, "nonesense", Float64)
+        @test_throws ErrorException gather(lmp, "c_nonsense", Float64)
+        @test_throws ErrorException gather(lmp, "f_nonesense", Float64)
+
+        @test_throws AssertionError gather(lmp, "x", Float64, subset_bad1)
+        @test_throws AssertionError gather(lmp, "x", Float64, subset_bad2)
+
+        @test_throws ErrorException scatter!(lmp, "nonesense", data)
+        @test_throws ErrorException scatter!(lmp, "c_nonsense", data)
+        @test_throws ErrorException scatter!(lmp, "f_nonesense", data)
+
+        @test_throws AssertionError scatter!(lmp, "x", subset_bad_data, subset_bad1)
+        @test_throws AssertionError scatter!(lmp, "x", subset_bad_data, subset_bad2)
+
+        @test gather(lmp, "x", Float64) == gather(lmp, "c_pos", Float64) == gather(lmp, "f_pos", Float64)
+
+        @test gather(lmp, "x", Float64)[:,subset] == gather(lmp, "x", Float64, subset)
+        @test gather(lmp, "c_pos", Float64)[:,subset] == gather(lmp, "c_pos", Float64, subset)
+        @test gather(lmp, "f_pos", Float64)[:,subset] == gather(lmp, "f_pos", Float64, subset)
+
+        scatter!(lmp, "x", data)
+        scatter!(lmp, "f_pos", data)
+        scatter!(lmp, "c_pos", data)
+
+        @test gather(lmp, "x", Float64) == gather(lmp, "c_pos", Float64) == gather(lmp, "f_pos", Float64) == data
+
+        scatter!(lmp, "x", data_subset, subset)
+        scatter!(lmp, "c_pos", data_subset, subset)
+        scatter!(lmp, "f_pos", data_subset, subset)
+
+        @test gather(lmp, "x", Float64, subset) == gather(lmp, "c_pos", Float64, subset) == gather(lmp, "f_pos", Float64, subset) == data_subset
+
+    end
+end
+
 @test success(pipeline(`$(MPI.mpiexec()) -n 2 $(Base.julia_cmd()) mpitest.jl`, stderr=stderr, stdout=stdout))
