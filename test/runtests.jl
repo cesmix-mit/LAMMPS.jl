830158f5c86aa951c345fd72fcb5c4f9dc89b699
Joroks <johannes@roksvaag.com>
2024-06-26 17:24:15 +0200

    adjust tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,26 +17,28 @@ end
 
 @testset "Variables" begin
     LMP(["-screen", "none"]) do lmp
-        command(lmp, "box tilt large")
-        command(lmp, "region cell block 0 1.0 0 1.0 0 1.0 units box")
-        command(lmp, "create_box 1 cell")
-        command(lmp, "create_atoms 1 random 10 1 NULL")
-        command(lmp, "compute  press all pressure NULL pair");
-        command(lmp, "fix press all ave/time 1 1 1 c_press mode vector");
+        command(lmp, """
+            box tilt large
+            region cell block 0 1.0 0 1.0 0 1.0 units box
+            create_box 1 cell
+            create_atoms 1 random 10 1 NULL
+            compute  press all pressure NULL pair
+            fix press all ave/time 1 1 1 c_press mode vector
 
-        command(lmp, "variable var1 equal 1.0")
-        command(lmp, "variable var2 string \"hello\"")
-        command(lmp, "variable var3 atom x")
-        # TODO: x is 3d, how do we access more than the first dims
-        command(lmp, "variable var4 vector f_press")
+            variable var1 equal 1.0
+            variable var2 string \"hello\"
+            variable var3 atom x
+            # TODO: x is 3d, how do we access more than the first dims
+            variable var4 vector f_press
+        """)
 
-        @test LAMMPS.extract_variable(lmp, "var1") == 1.0
-        @test LAMMPS.extract_variable(lmp, "var2") == "hello"
-        x = LAMMPS.extract_atom(lmp, "x")
-        x_var = LAMMPS.extract_variable(lmp, "var3")
+        @test extract_variable(lmp, "var1", VARIABLE_EQUAL) == 1.0
+        @test extract_variable(lmp, "var2", VARIABLE_STRING) == "hello"
+        x = extract_atom(lmp, "x", LAMMPS_DOUBLE_2D)
+        x_var = extract_variable(lmp, "var3", VARIABLE_ATOM)
         @test length(x_var) == 10
         @test x_var == x[1, :]
-        press = LAMMPS.extract_variable(lmp, "var4")
+        press = LAMMPS.extract_variable(lmp, "var4", VARIABLE_VECTOR)
         @test press isa Vector{Float64}
     end
 end
@@ -44,17 +46,20 @@ end
 @testset "gather/scatter" begin
     LMP(["-screen", "none"]) do lmp
         # setting up example data
-        command(lmp, "atom_modify map yes")
-        command(lmp, "region cell block 0 3 0 3 0 3")
-        command(lmp, "create_box 1 cell")
-        command(lmp, "lattice sc 1")
-        command(lmp, "create_atoms 1 region cell")
-        command(lmp, "mass 1 1")
+        command(lmp, """
+            atom_modify map yes
+            region cell block 0 3 0 3 0 3
+            create_box 1 cell
+            lattice sc 1
+            create_atoms 1 region cell
+            mass 1 1
 
-        command(lmp, "compute pos all property/atom x y z")
-        command(lmp, "fix pos all ave/atom 10 1 10 c_pos[1] c_pos[2] c_pos[3]")
+            compute pos all property/atom x y z
+            fix pos all ave/atom 10 1 10 c_pos[*]
 
-        command(lmp, "run 10")
+            run 10
+        """)
+        
         data = zeros(Float64, 3, 27)
         subset = Int32.([2,5,10, 5])
         data_subset = ones(Float64, 3, 4)
@@ -63,15 +68,15 @@ end
         subset_bad2 = Int32.([0])
         subset_bad_data = ones(Float64, 3,1)
 
-        @test_throws AssertionError gather(lmp, "x", Int32)
-        @test_throws AssertionError gather(lmp, "id", Float64)
+        @test_throws AssertionError gather(lmp, "x", LAMMPS_INT_2D)
+        @test_throws AssertionError gather(lmp, "id", LAMMPS_DOUBLE)
 
-        @test_throws ErrorException gather(lmp, "nonesense", Float64)
-        @test_throws ErrorException gather(lmp, "c_nonsense", Float64)
-        @test_throws ErrorException gather(lmp, "f_nonesense", Float64)
+        @test_throws ErrorException gather(lmp, "nonesense", LAMMPS_DOUBLE_2D)
+        @test_throws ErrorException gather(lmp, "c_nonsense", LAMMPS_DOUBLE_2D)
+        @test_throws ErrorException gather(lmp, "f_nonesense", LAMMPS_DOUBLE_2D)
 
-        @test_throws AssertionError gather(lmp, "x", Float64, subset_bad1)
-        @test_throws AssertionError gather(lmp, "x", Float64, subset_bad2)
+        @test_throws AssertionError gather(lmp, "x", LAMMPS_DOUBLE_2D, subset_bad1)
+        @test_throws AssertionError gather(lmp, "x", LAMMPS_DOUBLE_2D, subset_bad2)
 
         @test_throws ErrorException scatter!(lmp, "nonesense", data)
         @test_throws ErrorException scatter!(lmp, "c_nonsense", data)
@@ -80,23 +85,23 @@ end
         @test_throws AssertionError scatter!(lmp, "x", subset_bad_data, subset_bad1)
         @test_throws AssertionError scatter!(lmp, "x", subset_bad_data, subset_bad2)
 
-        @test gather(lmp, "x", Float64) == gather(lmp, "c_pos", Float64) == gather(lmp, "f_pos", Float64)
+        @test gather(lmp, "x", LAMMPS_DOUBLE_2D) == gather(lmp, "c_pos", LAMMPS_DOUBLE_2D) == gather(lmp, "f_pos", LAMMPS_DOUBLE_2D)
 
-        @test gather(lmp, "x", Float64)[:,subset] == gather(lmp, "x", Float64, subset)
-        @test gather(lmp, "c_pos", Float64)[:,subset] == gather(lmp, "c_pos", Float64, subset)
-        @test gather(lmp, "f_pos", Float64)[:,subset] == gather(lmp, "f_pos", Float64, subset)
+        @test gather(lmp, "x", LAMMPS_DOUBLE_2D)[:,subset] == gather(lmp, "x", LAMMPS_DOUBLE_2D, subset)
+        @test gather(lmp, "c_pos", LAMMPS_DOUBLE_2D)[:,subset] == gather(lmp, "c_pos", LAMMPS_DOUBLE_2D, subset)
+        @test gather(lmp, "f_pos", LAMMPS_DOUBLE_2D)[:,subset] == gather(lmp, "f_pos", LAMMPS_DOUBLE_2D, subset)
 
         scatter!(lmp, "x", data)
         scatter!(lmp, "f_pos", data)
         scatter!(lmp, "c_pos", data)
 
-        @test gather(lmp, "x", Float64) == gather(lmp, "c_pos", Float64) == gather(lmp, "f_pos", Float64) == data
+        @test gather(lmp, "x", LAMMPS_DOUBLE_2D) == gather(lmp, "c_pos", LAMMPS_DOUBLE_2D) == gather(lmp, "f_pos", LAMMPS_DOUBLE_2D) == data
 
         scatter!(lmp, "x", data_subset, subset)
         scatter!(lmp, "c_pos", data_subset, subset)
         scatter!(lmp, "f_pos", data_subset, subset)
 
-        @test gather(lmp, "x", Float64, subset) == gather(lmp, "c_pos", Float64, subset) == gather(lmp, "f_pos", Float64, subset) == data_subset
+        @test gather(lmp, "x", LAMMPS_DOUBLE_2D, subset) == gather(lmp, "c_pos", LAMMPS_DOUBLE_2D, subset) == gather(lmp, "f_pos", LAMMPS_DOUBLE_2D, subset) == data_subset
 
     end
 end
