563ea922d3b96c584b2065f7a59dbc1168673fc5
Joroks <johannes@roksvaag.com>
2024-07-01 09:53:55 +0200

    fix test on windows-latest

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using MPI
 
 @test_logs (:warn,"LAMMPS library path changed, you will need to restart Julia for the change to take effect") LAMMPS.set_library!(LAMMPS.locate())
 
-LMP() do lmp
+LMP(["-screen", "none"]) do lmp
     @test LAMMPS.version(lmp) >= 0
 end
 
