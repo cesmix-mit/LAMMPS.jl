ea4c386b0298e5eb6594d721dc62392811f5e1e7
Joroks <johannes@roksvaag.com>
2025-05-12 16:29:46 +0200

    add optimistic implementation

--- benchmark/bench_lj.jl
+++ benchmark/bench_lj.jl
@@ -3,6 +3,8 @@ module BenchmarkLJ
 using BenchmarkTools
 using MPI; MPI.Init()
 using LAMMPS
+using StaticArrays
+using LinearAlgebra
 import Enzyme, ForwardDiff
 import ADTypes: AutoEnzyme, AutoForwardDiff
 
@@ -93,12 +95,75 @@ function setup_external(backend)
     return lmp
 end
 
+function setup_optimistic()
+    lmp = LMP(["-screen", "none", "-log", "none"])
+
+    command(lmp, raw"""
+        newton off
+        variable        x index 1
+        variable        y index 1
+        variable        z index 1
+
+        variable        xx equal 20*$x
+        variable        yy equal 20*$y
+        variable        zz equal 20*$z
+
+        units           lj
+        atom_style      atomic
+
+        lattice         fcc 0.8442
+        region          box block 0 ${xx} 0 ${yy} 0 ${zz}
+        create_box      1 box
+        create_atoms    1 box
+        mass            1 1.0
+
+        velocity        all create 1.44 87287 loop geom
+
+        neighbor        0.3 bin
+        neigh_modify    delay 0 every 20 check no
+
+        pair_style zero 2.5 nocoeff
+        pair_coeff * *
+    """)
+
+    FixExternal(lmp, "pair_julia", "all", 1, 1) do fix
+        x = reinterpret(reshape, SVector{3, Float64}, fix.x)
+        f = reinterpret(reshape, SVector{3, Float64}, fix.f)
+
+        fill!(f, zero(eltype(f)))
+
+        @inbounds for (i, neigh) in pair_neighborlist(fix.lmp, "zero")
+            ipos = x[i]
+
+            for j in neigh
+                diff = ipos - x[j]
+                rsqr = diff ⋅ diff
+                rsqr > 6.25 && continue
+
+                r2inv = inv(rsqr)
+                r6inv = r2inv^3
+                force = diff * (24*r6inv * (48*r6inv - 1) * r2inv)
+
+                f[i] += force
+
+                if j <= fix.nlocal
+                    f[j] -= force
+                end
+            end
+        end
+    end
+
+    command(lmp, "fix 1 all nve")
+    return lmp
+end
+
 suite = BenchmarkGroup()
 
 suite["Reference"] = @benchmarkable command(lmp, "run 100") setup = (lmp = setup_reference())
 suite["ForwardDiff"] = @benchmarkable command(lmp, "run 100") setup = (lmp = setup_external(AutoForwardDiff()))
 suite["Enzyme"] = @benchmarkable command(lmp, "run 100") setup = (lmp = setup_external(AutoEnzyme()))
 suite["Manual"] = @benchmarkable command(lmp, "run 100") setup = (lmp = setup_external(nothing))
+suite["Optimistic"] = @benchmarkable command(lmp, "run 100") setup = (lmp = setup_optimistic())
 
 end
 BenchmarkLJ.suite
