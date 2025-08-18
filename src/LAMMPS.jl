2303e68d8bf196349c51d01c0496af08d98640f0
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-08-18 13:09:45 -0400

    add tests

--- src/LAMMPS.jl
+++ src/LAMMPS.jl
@@ -848,11 +848,12 @@ function gather!(lmp::LMP, name::String, data::AbstractMatrix{T}, ids::Union{Not
     natoms = get_natoms(lmp)
     ndata = isnothing(ids) ? natoms : length(ids)
 
-    if !_array_stride_valid(x)
-        throw(ArgumentError("x must be contiguous in memory (i.e., interpretable as a 1D array)"))
+    if !_array_stride_valid(data)
+        throw(ArgumentError("data must be contiguous in memory (i.e., interpretable as a 1D array)"))
     end
-    if size(x) == (count, ndata)
-        throw(ArgumentError("Dimension of data must be $(count) x $(ndata) for name $name. Got $(size(data))"))
+
+    if size(data) != (count, ndata)
+        throw(ArgumentError("Dimension of provided storage must be $(count) x $(ndata) for name $name. Got $(size(data))"))
     end
 
     return _gather!(lmp, name, data, ids, count, natoms, ndata)
