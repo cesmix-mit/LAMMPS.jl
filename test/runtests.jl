d6e0a2240ea2728b8b7b6eb18fed5c09863985bc
Joroks <32484985+Joroks@users.noreply.github.com>
2025-08-20 13:22:39 +0200

    Add some small functions (#94)
    * add `eval` function
    
    * add `file` command

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,13 @@ LMP(["-screen", "none"]) do lmp
     @test LAMMPS.version(lmp) >= 0
     command(lmp, "clear")
 
-    @test_throws LAMMPSError command(lmp, "nonsense")
+    @test_throws LAMMPSError command(lmp, "nonesense")
+    @test_throws LAMMPSError LAMMPS.eval(lmp, "nonesense")
+    @test LAMMPS.eval(lmp, "1+1") == 2
+    
+    LAMMPS.file(lmp, "test_files/test.lmp")
+    @test LAMMPS.eval(lmp, "v_var") == 1
+    @test_throws LAMMPSError LAMMPS.file(lmp, "nonesense")
 
     LAMMPS.close!(lmp)
 
