31d03c2eef34e71c7fb13227bbd962180242fd9c
Joroks <johannes@roksvaag.com>
2025-08-20 11:30:48 +0200

    cleanup

--- docs/src/api.md
+++ docs/src/api.md
@@ -5,28 +5,28 @@ Modules = [LAMMPS]
 Pages = ["LAMMPS.jl"]
 ```
 
-# extract API
+## extract API
 
 ```@autodocs
 Modules = [LAMMPS]
 Pages = ["extract.jl"]
 ```
 
-# gather/scatter operations
+## gather/scatter operations
 
 ```@autodocs 
 Modules = [LAMMPS]
 Pages = ["gather_scatter.jl"]
 ```
 
-# neighbor list access
+## neighbor list access
 
 ```@autodocs
 Modules = [LAMMPS]
 Pages = ["neighborlist.jl"]
 ```
 
-# FixExternal
+## FixExternal
 !!! danger "experimental feature"
     The `FixExternal` and `PairExternal` API is highly experimental and is subject to major changes in the future
 
--- src/LAMMPS.jl
+++ src/LAMMPS.jl
@@ -148,11 +148,20 @@ function set_library!(path)
 end
 
 """
-    LMP(args::Vector{String}=String[], comm::MPI.Comm=MPI.COMM_WORLD)
+    LMP([f::Function,] args::Vector{String}=String[], comm=MPI.COMM_WORLD)
 
 Create a new LAMMPS instance while passing in a list of strings as if they were command-line arguments for the LAMMPS executable.
 
 A full ist of command-line options can be found in the [lammps documentation](https://docs.lammps.org/Run_options.html).
+
+!!! info "MPI"
+    If your lammps binary is build with MPI, you're required to call `MPI.Init()` before creating a lammps instance.
+
+```julia
+LMP(["-log", "none"]) do lmp
+    command(lmp, "print \\"created a new lammps instance\\"")
+end
+```
 """
 mutable struct LMP
     @atomic handle::Ptr{Cvoid}
@@ -192,6 +201,12 @@ mutable struct LMP
     end
 end
 
+function LMP(f::Function, args=String[], comm=MPI.COMM_WORLD)
+    lmp = LMP(args, comm)
+    return f(lmp)
+    # `close!` is registered as a finalizer for LMP, no need to close it here.
+end
+
 function Base.cconvert(::Type{Ptr{Cvoid}}, lmp::LMP)    
     lmp.handle == C_NULL && error("The LMP object doesn't point to a valid LAMMPS instance! "
             * "This is usually caused by calling `LAMMPS.close!` or through serialization and deserialization.")
@@ -199,11 +214,6 @@ function Base.cconvert(::Type{Ptr{Cvoid}}, lmp::LMP)
 end
 Base.unsafe_convert(::Type{Ptr{Cvoid}}, lmp::LMP) = lmp.handle
 
-"""
-    close!(lmp::LMP)
-
-Shutdown a LAMMPS instance.
-"""
 function close!(lmp::LMP)
     handle = @atomicswap lmp.handle = C_NULL
     if handle !== C_NULL 
@@ -211,17 +221,6 @@ function close!(lmp::LMP)
         API.lammps_close(handle)
     end
     return nothing
-end
-
-"""
-    LMP(f::Function, args=String[], comm=MPI.COMM_WORLD)
-
-Create a new LAMMPS instance and call `f` on that instance while returning the result from `f`.
-"""
-function LMP(f::Function, args=String[], comm=MPI.COMM_WORLD)
-    lmp = LMP(args, comm)
-    return f(lmp)
-    # `close!` is registered as a finalizer for LMP, no need to close it here.
 end
 
 function version(lmp::LMP)
@@ -277,10 +276,6 @@ Those lines are combined by removing the ‘&’ and the following newline chara
 After this processing the string is handed to LAMMPS for parsing and executing.
 
 Arrays of Strings get concatenated into a single String inserting newline characters as needed.
-
-!!! compat "LAMMPS.jl 0.4.1"
-    Multiline string support `\"""` and support for array of strings was added.
-    Prior versions of LAMMPS.jl ignore newline characters.
 
 # Examples
 
